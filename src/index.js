3b2b3d5b74cbdae8af546e2f78ad62cd5d44fae9
PiachkoAliaksey <alek17y@mail.ru>
2022-05-08 17:50:01 +0300

    feat: add change languages ru/en

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ let blockTextarea=document.createElement('textarea');
 blockTextarea.className='blockTextarea';//rows="4" cols="50" placeholder="Describe yourself here..."
 blockTextarea.rows='5';
 blockTextarea.cols='50';
-blockTextarea.placeholder='Press keyboard...'
 blockDiv.appendChild(blockTextarea);
 
 let blockChild=document.createElement('div');
@@ -25,7 +24,7 @@ blockDiv.appendChild(blockInformation);
 
 let blockRuEn=document.createElement('p');
 blockRuEn.className='blockInformation';
-blockRuEn.textContent='Для переключения языка комбинация: левые Ctrl + Alt';
+blockRuEn.textContent='Для переключения языка комбинация: левые Shift + Alt';
 blockDiv.appendChild(blockRuEn);
 
 let arrNumKeys=['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\','DEL','CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/','&#129045' ,'Shift', 'Ctrl', 'WIN', 'Alt', ' ', 'Alt', 'Ctrl', '&#129044', '&#129047','&#129046'];
@@ -39,6 +38,7 @@ let arrCode=['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Dig
 
 function buildKeyboard(){
     let board='';
+    
     for(let i=0;i<arrNumKeys.length;i++){
         
             if(arrNumKeys[i]==='Backspace'||arrNumKeys[i]==='CapsLock'||arrNumKeys[i]==='Enter'||arrNumKeys[i]==='Shift'){
@@ -55,7 +55,7 @@ function buildKeyboard(){
                 if(i==14||i==29||i==42||i==55){
                     board+='<div class="clearBlock"></div>'
                 }
-                board+=`<div class="keyB"> ${arrNumKeys[i]} </div>`;
+                board+=`<div class="keyB">${arrNumKeys[i]}</div>`;
             }  
     }
     document.querySelector('.blockChild').innerHTML=board;
@@ -63,7 +63,7 @@ function buildKeyboard(){
 buildKeyboard();
 
 function addData(){
-   let child=document.querySelectorAll('.keyB, .keyBcs, .keyBcsSpace')
+   let child=document.querySelectorAll('.keyB, .keyBcs, .keyBcsSpace');
    for (let i=0;i<arrCode.length;i++){
        if(!(child[i].hasAttribute('data'))){
            child[i].setAttribute('data',arrCode[i])
@@ -72,13 +72,52 @@ function addData(){
 }
 addData();
 
-
+let textArea=document.querySelector('.blockTextarea');
+let textPress=[];
+let flag=false;
+let arrRu=['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\','DEL','CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', "э", 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '/','&#129045' ,'Shift', 'Ctrl', 'WIN', 'Alt', ' ', 'Alt', 'Ctrl', '&#129044', '&#129047','&#129046']
 document.onkeydown=function(event){
+
+    if(event.code==='ShiftLeft') flag=true;
+    if(flag&&event.code==='AltLeft'){
+        flag=false;
+        let textBtnK=document.querySelectorAll('.keyB, .keyBcs, .keyBcsSpace');
+        if(textBtnK[0].textContent !==arrRu[0]){
+            for(let i=0;i<arrRu.length;i++){
+                textBtnK[i].textContent=`${arrRu[i]}`;
+            }
+
+        }else{
+            for(let i=0;i<arrNumKeys.length;i++){
+                textBtnK[i].textContent=`${arrNumKeys[i]}`;
+            }
+        }
+      
+
+    }   
+
+    
+
+    if(event.key.length===1){
+        textPress.push(event.key);
+    }
+    if(event.code==='Backspace'){
+        textPress.pop()
+    }
+    if(event.code==='Space'){
+        textPress.push('')
+    }
+    /*if(event.code==='Tab'){
+        textPress.push('')
+    }*/
+
+    textArea.textContent=textPress.join('');
+
     //console.log(event.code);
     //console.log(event.key);
     document.onkeyup=function(event){
         if(event.code==='Backspace'||event.code==='CapsLock'|| event.code==='ShiftLeft'||event.code==='ShiftRight'||event.code==='Enter'){
-            document.querySelector('.keyBcs[data="'+ event.code +'"]').classList.remove('btnActive');
+            document.querySelector('.keyBcs[data="'+ event.code +'"]').classList.remove('btnActive'); 
     
         }else if(event.code==='Space'){
             document.querySelector('.keyBcsSpace[data="'+ event.code +'"]').classList.remove('btnActive');
@@ -86,11 +125,21 @@ document.onkeydown=function(event){
             document.querySelector('.keyB[data="'+ event.code +'"]').classList.remove('btnActive');
     
         }
-        
+       
     }
     
     if(event.code==='Backspace'||event.code==='CapsLock'|| event.code==='ShiftLeft'||event.code==='ShiftRight'||event.code==='Enter'){
         document.querySelector('.keyBcs[data="'+ event.code +'"]').classList.add('btnActive');
+        if(event.code==='CapsLock'){
+            document.querySelectorAll('.keyB').forEach((val)=>{
+                if(val.textContent.length===1 && val.textContent !==val.textContent.toLocaleUpperCase()){
+                    val.textContent=val.textContent.toLocaleUpperCase()
+                }else  if(val.textContent.length===1 && val.textContent !==val.textContent.toLocaleLowerCase()){
+                    val.textContent=val.textContent.toLocaleLowerCase()
+                }
+
+            })
+        } 
 
     }else if(event.code==='Space'){
         document.querySelector('.keyBcsSpace[data="'+ event.code +'"]').classList.add('btnActive');
@@ -98,28 +147,33 @@ document.onkeydown=function(event){
         document.querySelector('.keyB[data="'+ event.code +'"]').classList.add('btnActive');
 
     }
+
+  
+
+
+
 };
 
-document.querySelectorAll('.keyB, .keyBcs, .keyBcsSpace').forEach(function(val){
-    val.onclick=function(event){
-        document.querySelectorAll('.keyB, .keyBcs, .keyBcsSpace').forEach((val)=>{
-            val.classList.remove('btnActive');
-        });
-
-        let eventCode=this.getAttribute('data');
-        this.classList.add('btnActive');
-    }
-})
 
 let btnKeys=document.querySelectorAll('.keyB, .keyBcs, .keyBcsSpace');
 btnKeys.forEach((val)=>{
     val.addEventListener('mouseover',function(){
         val.classList.add('btnActiveDark');
-        val.addEventListener('mouseout',function(){
+            val.addEventListener('mousedown',function(){
+            val.classList.add('btnActive')
             val.classList.remove('btnActiveDark');
+            
+                textPress.push(val.textContent);
+            
+            val.addEventListener('mouseup',function(){
+                val.classList.remove('btnActive');
+                val.classList.add('btnActiveDark');
         })
+    
+      })
+       
+    })
+    val.addEventListener('mouseout',function(){
+        val.classList.remove('btnActiveDark');
     })
 })
-
-
-
